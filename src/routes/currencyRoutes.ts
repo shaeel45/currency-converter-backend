33f678b67ed9b717bf4fb93bd48c8b9626e9cb5a
shaeel45 <beingshaeel@gmail.com>
2025-08-29 17:07:46 +0500

    Controller/Routes Remove

--- src/routes/currencyRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import getConversion from "../controllers/currencyController.js";
-
-const router = Router();
-
-router.get("/", getConversion);
-
-export default router;
